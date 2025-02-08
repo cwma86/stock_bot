62cbce2cf1214931e503502f7a047e15362229bb
Cory W. Mauer <corymauer@gmail.com>
2025-02-07 18:23:41 -0700

    initial commit with basic functionality using hardcoded GPU urls

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Stock bot
+# Description
+Simple bot for checking stock of nvidia gpu's on the best buy website
+
+# Configure environement
+set email `EMAIL` and password `EMAIL_PASSWORD` credentials  (expects gmail) using environement variables. 
+
+
---
+++ stock_bot.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+"""
+Description of the script
+"""
+
+
+import argparse
+import logging
+import os
+import requests
+import time
+from bs4 import BeautifulSoup
+import smtplib
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'
+)
+
+class Product:
+    def __init__(
+        self,
+        title,
+        sku,
+        url
+    ):
+        self.title = title
+        self.sku = sku
+        self.url = url
+        
+def send_notification(message_body):
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    # start TLS for security
+    s.starttls()
+    # Authentication
+    email = os.environ.get('EMAIL') 
+    email_password = os.environ.get('EMAIL_PASSWORD')
+    s.login(email, email_password)
+    # message to be sent
+    # sending the mail
+    message_subject = "In Stock item Alert!"
+    message = 'Subject: {}\n\n{}'.format(message_subject, message_body)
+
+    s.sendmail(email, email, message)
+    # terminating the session
+    s.quit()
+    time.sleep(60*60) # We have been alerted for an in stock item, sleep for the next hour
+
+def checkBestBuy():
+    
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+
+    # Extracting the title of the page
+    item_list = [
+        Product("5080", "6614153", "https://www.bestbuy.com/site/nvidia-geforce-rtx-5080-16gb-gddr7-graphics-card-gun-metal/6614153.p?skuId=6614153"),
+        Product("5080 Gigabyte", "6615925", "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-5080-gaming-oc-16g-gddr7-pci-express-5-0-graphics-card-black/6615925.p?skuId=6615925"),
+        Product("5080 MSI", "6615227", "https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-5080-16g-gaming-trio-oc-16gb-gddr7-pci-express-gen-5-graphics-card-black/6615227.p?skuId=6615227"),
+        Product("5090 Gigabyte", "6615930", "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-5090-windforce-oc-32g-gddr7-pci-express-5-0-graphics-card-black/6615930.p?skuId=6615930"),
+        # Product("3060", "6468931", "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3060-12gb-gddr6-pci-express-4-0-graphics-card-black/6468931.p?skuId=6468931"),
+        # Product("ASUS 3060", "6557544", "https://www.bestbuy.com/site/asus-nvidia-geforce-rtx-3060-dual-overclock-12gb-gddr6-pci-express-4-0-graphics-card-black/6557544.p?skuId=6557544")
+    ]
+
+    for product in item_list:
+        response = requests.get(product.url, headers=headers)
+        soup = BeautifulSoup(response.content, "html5lib") # If this line causes an error, run 'pip install html5lib' or install html5lib
+        mybuttons = soup.find_all("button", {"data-sku-id": product.sku})
+        for button in mybuttons:
+            if button['data-button-state'] == "ADD_TO_CART":
+                logging.info(f"In stock item {soup.title.text}")
+                send_notification(f"In stock item {soup.title.text} \n {product.url}")
+
+def main():
+    """Main function of the script."""
+    parser = argparse.ArgumentParser(description="Description of the script")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG) # Set the logger's level to INFO
+        logging.debug("Logl level set to debug")
+
+    # Your script logic here
+    while True:
+        checkBestBuy()
+        time.sleep(10*60)  # Pause for 10 minutes
+    
+if __name__ == "__main__":
+    main()
